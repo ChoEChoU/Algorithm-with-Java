e159ead10646c380c32d03e76afda0ecc619c418
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-21 20:45:13 +0900

    Write base code in Entropy Class

--- Algorithm with Java/src/decisionTree/Entropy.java
+++ Algorithm with Java/src/decisionTree/Entropy.java
@@ -1,5 +1,18 @@
 package decisionTree;
 
 public class Entropy {
-
+	private Node firstNode;
+	private Node secondNode;
+	
+	public double getEntropy() {
+		return 0;
+	}
+	
+	public void setFirstNode(Node node) {
+		
+	}
+	
+	public void setSecondNode(Node node) {
+		
+	}
 }
