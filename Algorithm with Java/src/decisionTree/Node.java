6e16e0a934dfa5a8189e7bfa310e1d2a36030483
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-21 20:42:13 +0900

    Write base code in node class

--- Algorithm with Java/src/decisionTree/Node.java
+++ Algorithm with Java/src/decisionTree/Node.java
@@ -1,5 +1,13 @@
 package decisionTree;
 
 public class Node {
+	private Person person;
 
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
 }
