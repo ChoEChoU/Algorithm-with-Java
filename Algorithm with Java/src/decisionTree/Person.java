c5016be3ad170375c33e06c54e6a3142b36d4958
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-21 20:41:14 +0900

    Write base code in person class

--- Algorithm with Java/src/decisionTree/Person.java
+++ Algorithm with Java/src/decisionTree/Person.java
@@ -1,5 +1,39 @@
 package decisionTree;
 
 public class Person {
-
+	private String name;
+	private String gender;
+	private int hairLength;
+	private int weight;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getHairLength() {
+		return hairLength;
+	}
+	public void setHairLength(int hairLength) {
+		this.hairLength = hairLength;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
