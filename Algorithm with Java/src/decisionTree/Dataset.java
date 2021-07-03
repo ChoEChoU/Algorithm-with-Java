7c8e70ce5087ee659a544918e57adc49f2e52fd4
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-07-03 12:55:08 +0900

    Add DataSet class and write code

---
+++ Algorithm with Java/src/decisionTree/Dataset.java
@@ -0,0 +1,20 @@
+package decisionTree;
+
+import java.util.ArrayList;
+
+public class Dataset {
+	private ArrayList<Person> personSet;
+	
+	public Dataset() {
+		personSet = new ArrayList<Person>();
+	}
+	
+	public void addPersonSet(Person person) {
+		personSet.add(person);
+	}
+	
+	public ArrayList<Person> addPersonSet(){
+		return personSet;
+	}
+	
+}
