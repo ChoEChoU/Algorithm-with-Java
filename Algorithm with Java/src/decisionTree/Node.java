ab1e4666ac1f3571a16f87e00c607306155a4c9a
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-07-03 13:59:03 +0900

    Modify node class code

--- Algorithm with Java/src/decisionTree/Node.java
+++ Algorithm with Java/src/decisionTree/Node.java
@@ -4,12 +4,36 @@ import java.util.ArrayList;
 
 public class Node {
 	private ArrayList<Person> personSet;
+	
+	
 	private Node leftNode;
 	private Node rightNode;
 	
 	public Node() {
 		personSet = new ArrayList<Person>();
 	}
+
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
 	
+	public void addPersonSet(Person person) {
+		personSet.add(person);
+	}
 	
+	public ArrayList<Person> getPersonSet() {
+		return personSet;
+	}
 }
